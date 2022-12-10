839dc2223288ffb21d6ee6e7e2b0920319a1bac2
patolow <patolow@hotmail.com>
2022-12-10 09:41:09 -0300

    pato

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ app.use(express.static('public'));
 app.listen(port, () => console.log('listening'))
 
 app.get('/', (req, res) => {
-  let htmlPath = path.join(__dirname,'./views/home.html')
+  let htmlPath = path.join(__dirname,'/views/home.html')
   res.sendFile(htmlPath)
 })
 
+app.get('/register', (req, res) => {
+  let htmlPath = path.join(__dirname,'/views/register.html')
+  res.sendFile(htmlPath)
+})
 
+app.get('/login', (req, res) => {
+  let htmlPath = path.join(__dirname,'/views/login.html')
+  res.sendFile(htmlPath)
+})
